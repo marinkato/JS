521bab9b99260fac6a6bb1ce5e382fcccf30e2d8
marina toto <marinkato@gmail.com>
2020-10-20 13:43:01 +0200

    adding the tooglePlay functions using the ternary operator

--- JS_exercises/video player /scripts.js
+++ JS_exercises/video player /scripts.js
@@ -1,15 +1,28 @@
 // get elements and store them into an array
 
 const player = document.querySelector('.player');
-const video = player.querySelector('.viewer')
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('progress');
-const progressBar = player.querySelector('.progress__filled')
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]')
+const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 
 // build the functionality
+function togglePlay () {
+  const method = video.paused ? 'play' : 'pause';// paused is a property that lives on the video
+  video[method]();
+}
+
+// a simple way of doing the if / else statement
+// function togglePlay () {
+//   if (video.paused) {
+//   }
+//   else {
+//   video.pause();
+//   }
+// }
 
 // connect the eventListeners
 
