59fda9d08e54c3212529b3c8aa7f9d94cf640e67
marina toto <marinkato@gmail.com>
2020-10-20 13:26:17 +0200

    selecting elements and saving them in variables

---
+++ JS_exercises/video player /scripts.js
@@ -0,0 +1,15 @@
+// get elements and store them into an array
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('progress');
+const progressBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]')
+const ranges = player.querySelector('.player__slider');
+
+
+// build the functionality
+
+// connect the eventListeners
+
